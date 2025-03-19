49f21002aecf5330016d09bcf51b37a717c85ac7
phipsae <philip.krause88@yahoo.de>
2025-03-19 10:48:49 -0400

    remove _update overwrite and add transfer transferFrom override

--- packages/foundry/contracts/PredictionMarketToken.sol
+++ packages/foundry/contracts/PredictionMarketToken.sol
@@ -11,37 +11,39 @@ contract PredictionMarketToken is ERC20 {
     address public predictionMarket;
     address public liquidityProvider;
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        uint256 initialSupply,
-        address _liquidityProvider
-    ) ERC20(name, symbol) {
+    constructor(string memory name, string memory symbol, uint256 initialSupply, address _liquidityProvider)
+        ERC20(name, symbol)
+    {
         predictionMarket = msg.sender;
         liquidityProvider = _liquidityProvider;
         _mint(msg.sender, initialSupply);
     }
 
     function mint(address to, uint256 amount) external {
-        if (msg.sender != predictionMarket)
+        if (msg.sender != predictionMarket) {
             revert PredictionMarketToken__OnlyPredictionMarketCanMint();
+        }
         _mint(to, amount);
     }
 
     function burn(address from, uint256 amount) external {
-        if (msg.sender != predictionMarket)
+        if (msg.sender != predictionMarket) {
             revert PredictionMarketToken__OnlyPredictionMarketCanBurn();
+        }
         _burn(from, amount);
     }
 
-    function _update(
-        address from,
-        address to,
-        uint256 value
-    ) internal virtual override {
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        if (msg.sender == liquidityProvider) {
+            revert PredictionMarketToken__LiquidityProviderCantSell();
+        }
+        return super.transfer(to, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override returns (bool) {
         if (from == liquidityProvider) {
             revert PredictionMarketToken__LiquidityProviderCantSell();
         }
-        super._update(from, to, value);
+        return super.transferFrom(from, to, amount);
     }
 }
